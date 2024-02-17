267baf4561a51673715cf88ef891f3501f5815b8
Kakde Dnyaneshwar <118787516+Kakdednyaneshwar@users.noreply.github.com>
2024-02-17 11:26:56 -0500

    Two Number Addition

---
+++ Addition.java
@@ -0,0 +1,15 @@
+class Addition
+{
+	public static void main(String args[])
+	{
+	int no1;
+	int no2;
+	
+	no1=Integer.parseInt(args[0]);
+	no2=Integer.parseInt(args[1]);
+
+	int sum=no1+no2;
+	System.out.println("Addition no1&no2 is = "+sum);
+
+	}
+}
